a4277621d6111f1f15861fa54ef5166e771032a6
uArendt <jarendt66@me.com>
2016-09-13 12:52:55 -0400

    update tests to new swift syntax and lab complete

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -15,9 +15,78 @@ class ViewController: UIViewController {
     }
     
     
-   // Implement your functions here
+    func averageIsAbove75(one: Double, two: Double, three: Double) -> Bool {
+        let average = (one + two + three) / 3 > 75
+        return average
+    }
 
-    
+    func passwordCombo(username: String, password: Int) -> String {
+        
+        switch username {
+                    case "Jerry", "Elaine", "Michael":
+                            if password % 3 == 0 {
+                                    return "Welcome!"
+                                } else {
+                                    return "Access Denied"
+                                }
+                    default:
+                            return "Access Denied"
+                    }
+
+    }
+
+    func numberGenerator(a: String, b: Float) -> Float {
+        var number: Float = 0
+        
+        switch a {
+        case "1":
+            number = 1
+        case "2" :
+            number = 2
+        case "3" :
+            number = 3
+        case "4" :
+            number = 4
+        case "5" :
+            number = 5
+        default:
+            break
+        }
+        
+        switch (number, b) {
+        case (1...5, 10.5...15):
+            return number * b
+        case (1...5, 20...30.5):
+            return number * b
+        default:
+            return 0.0
+        }
+    }
+}      
+        
+
+//        if username == "Jerry" {
+//            if password % 3 == 0 {
+//                return "Welcome!"
+//            }
+//            //correctPass(password)
+//        } else if username == "Elaine" {
+//            if password % 3 == 0 {
+//                return "Welcome!"
+//            }
+//         } else if username == "Michael" {
+//            if password % 3 == 0 {
+//                return "Welcome!"
+//            }
+//        } else {
+//            return "Access Denied"
+//        }
+        
+        
+//        if (a == "1" || a == "2" || a == "3" || a == "4" || a == "5")
+//            && ((b >= 10.5 && b <= 15.0) || (b >= 20.0 && b <= 30.5)){
+//            let x = Float(a)! + b
+//            return x
+//        }
 
 
-}
--- AllTheBasicsTests/VCTests.swift
+++ AllTheBasicsTests/VCTests.swift
@@ -24,13 +24,13 @@ class VCTests: QuickSpec {
         describe("averageIsAbove75(_:_:)") {
             it("Takes three numbers and, if their average is over 75, returns true, otherwise returns false") {
                 
-                expect( testVC.averageIsAbove75( 76.0, 75.0, 71.9 )).to(equal(false))    // NOTE we might want to get specific about the precision & rounding they should use (eg. is 75.4 > 75?  75.4 is *definitely* greater than 75.0)
+                expect( testVC.averageIsAbove75( 76.0, two: 75.0, three: 71.9 )).to(equal(false))    // NOTE we might want to get specific about the precision & rounding they should use (eg. is 75.4 > 75?  75.4 is *definitely* greater than 75.0)
                 
-                expect( testVC.averageIsAbove75( 75.0, 75.0, 75.0 )).to(equal(false))    // must be *OVER* 75
+                expect( testVC.averageIsAbove75( 75.0, two: 75.0, three: 75.0 )).to(equal(false))    // must be *OVER* 75
                 
-                expect( testVC.averageIsAbove75( 75.1, 75.0, 75.0 )).to(equal(true))   // they should be using Doubles, so this is *just* over 75
+                expect( testVC.averageIsAbove75( 75.1, two: 75.0, three: 75.0 )).to(equal(true))   // they should be using Doubles, so this is *just* over 75
                 
-                expect( testVC.averageIsAbove75( 80.0, 80.0, 80.0 )).to(equal(true))
+                expect( testVC.averageIsAbove75( 80.0, two: 80.0, three: 80.0 )).to(equal(true))
             }
         }
         
@@ -61,18 +61,18 @@ class VCTests: QuickSpec {
             
             it("Checks the validity of each paramters and returns their product as a double or, if there's a validation error, it returns 0") {
                 
-                expect(testVC.numberGenerator("3", 12)).to(equal(36.0))
+                expect(testVC.numberGenerator("3", b: 12)).to(equal(36.0))
                 
-                expect(testVC.numberGenerator("3", 12.5)).to(equal(37.5))  // make sure they're keeping precision
+                expect(testVC.numberGenerator("3", b: 12.5)).to(equal(37.5))  // make sure they're keeping precision
                 
-                expect(testVC.numberGenerator("3.5", 12)).to(equal(0.0))   // first arg is not a whole number
+                expect(testVC.numberGenerator("3.5", b: 12)).to(equal(0.0))   // first arg is not a whole number
                 
                 
-                expect(testVC.numberGenerator("7", 12)).to(equal(0.0))   // one arg out of range
+                expect(testVC.numberGenerator("7", b: 12)).to(equal(0.0))   // one arg out of range
                 
-                expect(testVC.numberGenerator("3", 8.1)).to(equal(0.0))  // other arg out of range
+                expect(testVC.numberGenerator("3", b: 8.1)).to(equal(0.0))  // other arg out of range
                 
-                expect(testVC.numberGenerator("6", 8)).to(equal(0.0))    // both args out of range
+                expect(testVC.numberGenerator("6", b: 8)).to(equal(0.0))    // both args out of range
                 
             }
         }
